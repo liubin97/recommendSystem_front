3b62668e165ab590b1292b274f005b0b0f1e85b6
liubint <liubint@yonyou.com>
2020-04-06 21:31:12 +0800

    用户管理电影管理页面

---
+++ WebMovieRecSystem/MovieRecSystemAdmin/src/js/data-tables/datatable-styling.js
@@ -0,0 +1,40 @@
+/*=========================================================================================
+    File Name: datatables-styling.js
+    Description: Styling Datatable
+    ----------------------------------------------------------------------------------------
+    Item Name: Convex - Bootstrap 4 HTML Admin Dashboard Template
+    Version: 1.0
+    Author: PIXINVENT
+    最新最全最好的Bootstrap模板：http://www.bootstrapmb.com
+==========================================================================================*/
+(function(window, document, $) {
+    'use strict';
+	$(document).ready(function() {
+
+	/********************************
+	*       js of Base style        *
+	********************************/
+
+	$('.base-style').DataTable();
+
+	/******************************
+	*       js of no style        *
+	******************************/
+
+	$('.no-style-no').DataTable();
+
+	/******************************************
+	*       js of Base style - compact        *
+	******************************************/
+
+	$('.compact').DataTable();
+
+	/*********************************
+	*       js of Bootstrap 3        *
+	**********************************/
+
+	$('.bootstrap-3').DataTable();
+
+
+	} );
+})(window, document, jQuery);
